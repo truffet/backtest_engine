5394911b1ee381b1fad43c2827545ebf0949c23e
sleepy <truffet22@gmail.com>
2019-11-26 17:51:03 +0100

    finished testing function to fetch a single bucket of trades, just need to work on return and think of a way to handle manual cursor lastTradeID

--- bitcoin/bitmex_perps/get1000Trades.py
+++ bitcoin/bitmex_perps/get1000Trades.py
@@ -1,6 +1,41 @@
 import requests
 
-def get1000Trades(timestamp, lastTradeID):
-	baseUrl = "https://www.bitmex.com/api/v1"
+def build_url(timestamp, baseUrl):
+	if timestamp != "":
+		return (baseUrl + "&startTime=" + timestamp)
+	return (baseUrl)
 
+def get_data(fullUrl):
+	try:
+		return (requests.get(fullUrl))
+	except Exception as e:
+		return ("Error: " + str(e))
+
+def toJson(response):
+	try:
+		return(response.json())
+	except Exception as e:
+		return ("Error: " + str(e))
+
+def get1000Trades(timestamp, lastTradeID):
+	baseUrl = "https://www.bitmex.com/api/v1/trade?symbol=XBT%3Aperpetual&count=1"
+	fullUrl = build_url(timestamp, baseUrl)
+	response = get_data(fullUrl)
+	data = toJson(response)
+
+	if (str(response).startswith("Error")):
+		print(1000)
+		print(type([response]))
+		print([response])
+	elif (str(data).startswith("Error")):
+		print(1001)
+		print(type([data]))
+		print([data])
+	else:
+		print(response)
+		print(response.status_code)
+		print(type(data))
+		print(data)
+
+#test
 get1000Trades("","")
