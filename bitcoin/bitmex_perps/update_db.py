58789ce7e5525c755ce93d15209912a504da3b9c
sleepy <truffet22@gmail.com>
2019-12-03 18:09:15 +0100

    fetching data from api OK, storing into db OK, need to work on the looping process by handling init params for the get1000trades fct

--- bitcoin/bitmex_perps/update_db.py
+++ bitcoin/bitmex_perps/update_db.py
@@ -2,6 +2,8 @@ from get1000Trades import get1000Trades
 from connect_to_db import connect_to_db
 from init import setup_env
 
+import pyarrow as pa
+import pandas as pd
 
 def count_rows(con):
 	cursor = con.cursor()
@@ -9,6 +11,32 @@ def count_rows(con):
 	nb_of_rows = list(count_rows)[0][0]
 	print("Number of rows in timeCursor: " + str(nb_of_rows))
 	return (nb_of_rows)
+
+def add_row_to_table(row, con):
+	print("Adding new row to tradesHistory")
+	df = pd.DataFrame({"tradeTime": [row["timestamp"]], "symbol": [row["symbol"]], "side": [row["side"]], "size": [row["size"]], "price": [row["price"]], "tickDirection": [row["tickDirection"]], "trdMatchID": [row["trdMatchID"]], "grossValue": [row["grossValue"]], "homeNotional": [row["homeNotional"]], "foreignNotional": [row["foreignNotional"]]}, columns=['tradeTime', 'symbol', 'side', 'size', 'price', 'tickDirection', 'trdMatchID', 'grossValue', 'homeNotional', 'foreignNotional'])
+	print(df)
+	con.load_table_columnar("tradesHistory", df, preserve_index=False)
+
+	
+def save_progress(row, con):
+	print("Saving progress to timeCursor")
+	query = "DELETE FROM timeCursor;"
+	con.execute(query)
+	df = pd.DataFrame({"tradeTime": [row["timestamp"]], "trdMatchID": [row["trdMatchID"]]}, columns=['tradeTime', 'trdMatchID'])
+	print(df)
+	con.load_table_columnar("timeCursor", df, preserve_index=False)
+
+
+def add_response_to_table(response, con):
+	del response[0]
+	size = len(response)
+	while (size > 0):
+		add_row_to_table(response[0], con)
+		if (size == 1):
+			save_progress(response[0], con)
+		del response[0]
+		size -=1
 
 
 #connect to omnisci bitmex database
@@ -34,14 +62,32 @@ while (True):
 
 	#fetch data from bitmex API
 	response = get1000Trades(startTime, lastTradeID)
-	print(str(response))
 
 	#handle responses:
 	if (str(response).startswith("Error")):
 		print(str(response))
 		break
-	elif (len(response) < 1001):
-		print("Less than a 1000 trades fetched, loading to database. Last response handled, end of update")
+	elif (response[0] != 200):
+		print(str(response))
 		break
 	else:
+		add_response_to_table(response, con)
+		if (len(response) < 1000):
+			print("Less than a 1000 trades fetched, loading to database. Last response handled, end of update")
+			break
 		print("1000 trades batch fetched, loading to database...")
+
+
+#
+# test code
+#
+
+cursor = con.cursor()
+query = cursor.execute("SELECT * FROM timeCursor;")
+display = list(query)
+print("timeCursor table:\n" + str(display))
+
+cursor = con.cursor()
+query = cursor.execute("SELECT * FROM tradesHistory;")
+display = list(query)
+print("tradesHistory table:\n" + str(display))
