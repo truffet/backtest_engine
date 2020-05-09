c4f90a55137057dfd7c6379d140d33a51f65c823
sleepy <truffet22@gmail.com>
2020-05-09 18:07:10 +0200

    added ignore rule for py cache and fixed dVolMA and dSMA % diff method by integrating concept of % increase and decrease

--- indicators/SMA/dsma.py
+++ indicators/SMA/dsma.py
@@ -5,4 +5,10 @@ def dsma(sma, data, index):
 		return(None)
 	d = float(Decimal(data[index][0])/Decimal(sma))
 	result = (d - 1) * 100
+	if (data[index][0] >= sma):
+		d = float(Decimal(data[index][0])/Decimal(sma))
+		result = (d - 1) * 100
+	else:
+		d = float(Decimal(sma)/Decimal(data[index][0]))
+		result = (d - 1) * -100
 	return(result)
--- indicators/VolumeMA/dVolMA_calculator.py
+++ indicators/VolumeMA/dVolMA_calculator.py
@@ -3,6 +3,11 @@ from decimal import *
 def dVolMA_calculator(VolMA, data, index):
 	if (VolMA == None):
 		return(None)
-	d = float(Decimal(data[index][0])/Decimal(VolMA))
-	result = (d - 1) * 100
+
+	if (data[index][0] >= VolMA):
+		d = float(Decimal(data[index][0])/Decimal(VolMA))
+		result = (d - 1) * 100
+	else:
+		d = float(Decimal(VolMA)/Decimal(data[index][0]))
+		result = (d - 1) * -100
 	return(result)
